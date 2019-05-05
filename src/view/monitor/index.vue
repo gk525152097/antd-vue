0175466f6fad709e1a72396afcb03543ddd977b9
韦相龙 <15366180053@189.com>
2019-05-05 15:44:53 +0800

    动态line制作

--- src/view/monitor/index.vue
+++ src/view/monitor/index.vue
@@ -48,7 +48,15 @@
           <div class="box left-box-item">
             <title-box title="活动情况预测">
               <template slot="body">
-                ss
+                <div class="echart-line">
+                  <div class="title">
+                    <p>目标评估</p>
+                    <h2>有望达到预期</h2>
+                  </div>
+                  <div class="echart">
+                    <charts :option="optionLine"/>
+                  </div>
+                </div>
               </template>
             </title-box>
           </div>
@@ -72,8 +80,8 @@
                   <div class="pie-item">
                     <div class="title">
                       <div>
-                        <p>测试</p>
-                        <h3>24%</h3>
+                        <p>中式快餐</p>
+                        <h3>28%</h3>
                       </div>
                     </div>
                     <charts :option="optionPie"/>
@@ -83,8 +91,8 @@
                   <div class="pie-item">
                     <div class="title">
                       <div>
-                        <p>测试</p>
-                        <h3>24%</h3>
+                        <p>西餐</p>
+                        <h3>22%</h3>
                       </div>
                     </div>
                     <charts :option="optionPie"/>
@@ -94,8 +102,8 @@
                   <div class="pie-item">
                     <div class="title">
                       <div>
-                        <p>测试</p>
-                        <h3>24%</h3>
+                        <p>火锅</p>
+                        <h3>32%</h3>
                       </div>
                     </div>
                     <charts :option="optionPie"/>
@@ -143,20 +151,57 @@ for (let i = 0; i < 12; i += 1) {
   salesData.y.push(Math.floor(Math.random() * 1000) + 200)
   salesData.barSmall2.push(Math.floor(Math.random() * 1000) + 200)
 }
+
+let now = +new Date(1997, 9, 3)
+let oneDay = 24 * 3600 * 1000
+let value = Math.random() * 1000
+
 export default {
   name: 'index',
   components: {
     'title-box': TitleBox,
     'charts': Charts
   },
+  mounted () {
+    for (let i = 0; i < 1000; i += 1) {
+      this.lineData.push(this.randomData())
+    }
+    setInterval(() => {
+      for (let i = 0; i < 5; i += 1) {
+        this.lineData.shift()
+        this.lineData.push(this.randomData())
+      }
+    }, 1000)
+  },
   data () {
     return {
+      lineData: [],
       optionLiquidFill: {
         series: [{
           type: 'liquidFill',
-          data: [0.6, 0.5, 0.4, 0.3],
-          radius: '80%'
+          data: [0.6, 0.55, 0.4, 0.25],
+          radius: '90%',
+          outline: {
+            show: false
+          },
+          backgroundStyle: {
+            borderColor: '#156ACF',
+            borderWidth: 1,
+            shadowColor: 'rgba(0, 0, 0, 0.4)',
+            shadowBlur: 20
+          },
+          shape: 'path://M367.855,428.202c-3.674-1.385-7.452-1.966-11.146-1.794c0.659-2.922,0.844-5.85,0.58-8.719 c-0.937-10.407-7.663-19.864-18.063-23.834c-10.697-4.043-22.298-1.168-29.902,6.403c3.015,0.026,6.074,0.594,9.035,1.728 c13.626,5.151,20.465,20.379,15.32,34.004c-1.905,5.02-5.177,9.115-9.22,12.05c-6.951,4.992-16.19,6.536-24.777,3.271 c-13.625-5.137-20.471-20.371-15.32-34.004c0.673-1.768,1.523-3.423,2.526-4.992h-0.014c0,0,0,0,0,0.014 c4.386-6.853,8.145-14.279,11.146-22.187c23.294-61.505-7.689-130.278-69.215-153.579c-61.532-23.293-130.279,7.69-153.579,69.202 c-6.371,16.785-8.679,34.097-7.426,50.901c0.026,0.554,0.079,1.121,0.132,1.688c4.973,57.107,41.767,109.148,98.945,130.793 c58.162,22.008,121.303,6.529,162.839-34.465c7.103-6.893,17.826-9.444,27.679-5.719c11.858,4.491,18.565,16.6,16.719,28.643 c4.438-3.126,8.033-7.564,10.117-13.045C389.751,449.992,382.411,433.709,367.855,428.202z',
+          label: {
+            normal: {
+              position: ['38%', '40%'],
+              textStyle: {
+                fontSize: 40,
+                color: '#D94854'
+              }
+            }
+          }
         }]
+
       },
       optionPie: {
         tooltip: {
@@ -235,7 +280,72 @@ export default {
             {name: '数据8', value: 632}
           ]
         }]
+      },
+      optionLine: {
+        tooltip: {
+          trigger: 'axis',
+          formatter: function (params) {
+            params = params[0]
+            let date = new Date(params.name)
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1]
+          },
+          axisPointer: {
+            animation: false
+          }
+        },
+        grid: {
+          top: '1%',
+          left: '1%',
+          right: '1%',
+          bottom: '1%'
+        },
+        xAxis: {
+          type: 'time',
+          splitLine: {
+            show: false
+          },
+          show: false
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%'],
+          splitLine: {
+            show: false
+          },
+          show: false
+        },
+        series: [{
+          name: '模拟数据',
+          type: 'line',
+          showSymbol: false,
+          hoverAnimation: false,
+          data: this.lineData,
+          areaStyle: {},
+          markLine: {
+            data: [
+              {type: 'average', name: '平均值'}
+            ]
+          }
+        }]
       }
+    }
+  },
+  methods: {
+    randomData () {
+      now = new Date(+now + oneDay)
+      value = value + Math.random() * 21 - 10
+      return {
+        name: now.toString(),
+        value: [
+          [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
+          Math.round(value)
+        ]
+      }
+    }
+  },
+  watch: {
+    lineData: function () {
+      this.optionLine.series[0].data = this.lineData
     }
   }
 }
@@ -300,5 +410,18 @@ export default {
 }
 .LiquidFill {
   height: 250px;
+}
+.echart-line {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  .title {
+    p, h2 {
+      margin: 0;
+    }
+  }
+  .echart {
+    flex: 1;
+  }
 }
 </style>
